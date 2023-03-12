1f2e2bb4eae5d4be9a7455e9254db27fc99cd432
auwal00836 <auwalshikutu@gmail.com>
2023-03-12 09:13:38 +0100

    files upload

---
+++ config/dBase.php
@@ -0,0 +1,65 @@
+<?php
+  
+  class dBase{
+    protected $dbname = 'otp_system';
+    protected $dbhost = 'localhost';
+    protected $dbpass = '';
+    protected $dbuser = 'root';
+    protected $handler, $stmt;
+
+   public function __construct() {
+
+        try 
+        {
+
+          $options = array(
+            PDO::ATTR_PERSISTENT =>true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+          );
+
+            $this->handler = new PDO("mysql:host=localhost;dbname=otp_system","root","", $options);
+            // echo "Connected";
+
+        } 
+        catch (PDOException $e) 
+        {
+            die($e->getMessage());
+            // echo "Failed to connect.";
+        }
+
+    }
+
+
+    /* 
+        for queries that return value - SELECT
+        the function returns the rows returned by the query
+    */
+    public function query($statement, $parameters_array)
+    {
+
+      $this->stmt = $this->handler->prepare($statement);
+      $this->stmt->execute($parameters_array);
+
+      return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+    /* 
+        for queries that do not return any value - UPDATE, DELETE and INSERT
+        the function returns the number of rows affected by the query
+    */
+    public function nonQuery($statement, $parameters_array)
+    {
+
+      $this->stmt = $this->handler->prepare($statement);
+      $this->stmt->execute($parameters_array);
+
+      return $this->stmt->rowCount();
+
+    }
+
+  
+  }
+
+
+ ?>
